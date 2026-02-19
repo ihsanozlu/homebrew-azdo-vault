55af19f757b4678787439873509a6505e5167552
ihsanozlu <87200172+ihsanozlu@users.noreply.github.com>
2026-02-19 09:15:18 +0300

    Bump azdo-vault version to 0.1.2 and update URLs

--- Formula/azdo-vault.rb
+++ Formula/azdo-vault.rb
@@ -1,25 +1,25 @@
 class AzdoVault < Formula
   desc "Cross-organization Azure DevOps backup and migration CLI"
   homepage "https://github.com/ihsanozlu/azdo-vault"
-  version "0.1.1"
+  version "0.1.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ihsanozlu/azdo-vault/releases/download/v0.1.1/azdo-vault-darwin-arm64"
-      sha256 "eecc010faed2e80e4ab873fc35ca562a23c73100e83ad3b5854aab8e1ea84e2c"
+      url "https://github.com/ihsanozlu/azdo-vault/releases/download/v0.1.2/azdo-vault-darwin-arm64"
+      sha256 "e1474758bdf57a8635e337d3954e158ff2977e0484ca6a85b551a559ec213bf5"
     else
-      url "https://github.com/ihsanozlu/azdo-vault/releases/download/v0.1.1/azdo-vault-darwin-amd64"
-      sha256 "2b52f61d157b4fcbb5636504cf8bc53d3bf6b1e99fa4d9df56867ab30840e53f"
+      url "https://github.com/ihsanozlu/azdo-vault/releases/download/v0.1.2/azdo-vault-darwin-amd64"
+      sha256 "29d84d0c3459ae3fe38288b65f0f9757241dddf8bd24258c72d89fe2585b2225"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ihsanozlu/azdo-vault/releases/download/v0.1.1/azdo-vault-linux-arm64"
-      sha256 "fa9a0140a1849519307daa657b7c7cd96e168e7d9a24a15d214a61f97590675f"
+      url "https://github.com/ihsanozlu/azdo-vault/releases/download/v0.1.2/azdo-vault-linux-arm64"
+      sha256 "d1d9e0f544279b86846353b9f4ded24c5f4dbf37b1b51ada211057f10c97b56b"
     else
-      url "https://github.com/ihsanozlu/azdo-vault/releases/download/v0.1.1/azdo-vault-linux-amd64"
-      sha256 "d78be6442be9751b4f185dd01b270de28e57751dd3dba59c3a640b1b4e344aa6"
+      url "https://github.com/ihsanozlu/azdo-vault/releases/download/v0.1.2/azdo-vault-linux-amd64"
+      sha256 "6a6b5cd369b476652daa8528c98af357dc34fd960c7686a0f57ba10ee1814657"
     end
   end
 
